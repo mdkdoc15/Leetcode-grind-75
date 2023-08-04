66e3d579943251daba57a87880ce1cbdbe46b8bf
Matthew Kight <70612402+mdkdoc15@users.noreply.github.com>
2023-08-04 11:32:37 -0400

    8/4/23 problems

--- MergeIntervals/MergeIntervals.py
+++ MergeIntervals/MergeIntervals.py
@@ -1,5 +1,27 @@
 # Created by matthewkight at 7/31/23
 
+from typing import List
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        i = 0
+        j = 1
+        out = []
+        while j < len(intervals):
+            if intervals[i][1] >= intervals[j][0]:
+                while intervals[i][1] >= intervals[j][0]:
+                    j += 1
+                out.append([intervals[i][0], intervals[j-1][1]])
+                i = j
+                j += 1
+            else:
+                out.append(intervals[i])
+                i += 1
+                j += 1
+        return out
 
 if __name__ == '__main__':
-    pass
+    s = Solution()
+    intv = [[1,3],[2,6],[8,10],[15,18]]
+    print(s.merge(intv))
